7f98ea73f70785900024f5db107a10695e750adb
mt_9 <hyunsichang@hanyang.ac.kr>
2021-01-16 16:23:09 +0900

    지하철역으로 서울 지도에 점 찍기

--- ordinary_day.R
+++ ordinary_day.R
@@ -4,7 +4,7 @@ library(raster)
 library(rgeos)
 library(maptools)
 library(rgdal)
-Sys.setlocale("LC_ALL","C")
+Sys.setlocale("LC_ALL",".1251")
 
 map <- shapefile("./map/TL_SCCO_SIG.shp")
 map <- spTransform(map, CRSobj = CRS('+proj=longlat + ellps=WGS84 +datum=WGS84 +no_defs'))
@@ -14,11 +14,17 @@ seoul_map <- new_map[new_map$id <= 11740,]
 
 station <- read.csv("./station_coordinate.csv",
                     na.strings = c("", " ", NA),
-                    header = TRUE,
                     as.is = TRUE,
                     encoding = "UTF-8")
-Sys.setlocale("LC_ALL","Korean")
 
-# ggplot() + geom_polygon(data = seoul_map, aes(x=long, y=lat, group = group),
-#                         fill='white', color='black')
-#           + geom_point(data=)
+congestion <- read.csv("./congestion.csv", na.strings = c("", " ", "#DIV/0!"), header = TRUE, encoding = "UTF-8")
+
+seoul_station_names <- unique(congestion["X.U.C5ED..U.BA85."])
+seoul_metro <- c('01호선','02호선', '03호선', '04호선', '05호선', '06호선', '07호선', '08호선')
+
+seoul_station <- subset(station, station$X.U.FEFF.line %in% seoul_metro)
+seoul_station <- subset(seoul_station,  seoul_station$name %in% seoul_station_names$X.U.C5ED..U.BA85.)
+
+ggplot() + geom_polygon(data = seoul_map, aes(x=long, y=lat, group = group),
+                        fill='white', color='black') + geom_point(data= seoul_station, aes(x=lng, y=lat, group=X.U.FEFF.line),
+                       fill='black', color='black')
