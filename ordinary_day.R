46b01b3afef8b754e8361b8997375f7a55814485
mt_9 <hyunsichang@hanyang.ac.kr>
2021-01-16 13:19:19 +0900

    지하철 csv -> 데이터프레임 불러옴

--- ordinary_day.R
+++ ordinary_day.R
@@ -4,6 +4,7 @@ library(raster)
 library(rgeos)
 library(maptools)
 library(rgdal)
+Sys.setlocale("LC_ALL","C")
 
 map <- shapefile("./map/TL_SCCO_SIG.shp")
 map <- spTransform(map, CRSobj = CRS('+proj=longlat + ellps=WGS84 +datum=WGS84 +no_defs'))
@@ -11,5 +12,13 @@ new_map <- fortify(map, region = 'SIG_CD')
 new_map$id <- as.numeric(new_map$id)
 seoul_map <- new_map[new_map$id <= 11740,]
 
-ggplot() + geom_polygon(data = seoul_map, aes(x=long, y=lat, group = group),
-                        fill='white', color='black')
+station <- read.csv("./station_coordinate.csv",
+                    na.strings = c("", " ", NA),
+                    header = TRUE,
+                    as.is = TRUE,
+                    encoding = "UTF-8")
+Sys.setlocale("LC_ALL","Korean")
+
+# ggplot() + geom_polygon(data = seoul_map, aes(x=long, y=lat, group = group),
+#                         fill='white', color='black')
+#           + geom_point(data=)
