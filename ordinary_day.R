9e414aa1f837cbbd7cf996e876a4080e4dc6830d
mt_9 <hyunsichang@hanyang.ac.kr>
2021-01-16 12:10:18 +0900

    지도 시각화 성공

--- ordinary_day.R
+++ ordinary_day.R
@@ -5,4 +5,11 @@ library(rgeos)
 library(maptools)
 library(rgdal)
 
-shapefile()
+map <- shapefile("./map/TL_SCCO_SIG.shp")
+map <- spTransform(map, CRSobj = CRS('+proj=longlat + ellps=WGS84 +datum=WGS84 +no_defs'))
+new_map <- fortify(map, region = 'SIG_CD')
+new_map$id <- as.numeric(new_map$id)
+seoul_map <- new_map[new_map$id <= 11740,]
+
+ggplot() + geom_polygon(data = seoul_map, aes(x=long, y=lat, group = group),
+                        fill='white', color='black')
